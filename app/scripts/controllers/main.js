9b638b63a81e083e3274004ba80ef2d885b31c6f
Vu Anh <brother.rain.1024@gmail.com>
2014-12-23 09:38:19 +0700

    add clone function, remove unnesscary promisses

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,6 +16,10 @@ angular.module('desktopAppApp')
 
 	$scope.tasks = {};
 
+	$scope.getClone = function(entity){
+		return _.clone(entity)
+	}
+
 	$scope.loadPromise = function(){
 		var defer = $q.defer();
 		$scope.model.find({}, function(err, data){
@@ -35,41 +39,21 @@ angular.module('desktopAppApp')
 
 	$scope.insert = function(item){
 
-		var defer = $q.defer();
 		$scope.model.insert(item, function(err, data){
 			$scope.newItem = {};
 			$scope.load();
-			defer.resolve();
 		});
-		return defer.promise; 
 	}
 
 	$scope.remove = function(item){
-		var defer = $q.defer();
-		console.log("delete task ", item);
 		$scope.model.remove({"_id" : item._id }, {}, function(err, data){ 
-			console.log("delete task successfully");
 			$scope.load();
-			defer.resolve();
 		})
-		return defer.promise; 
 	}
 
 	$scope.update = function(item){
-		var defer = $q.defer();
-		console.log("update task ", item);
 		$scope.model.update({"_id" : item._id}, {$set: item}, {}, function(err, data){
 			$scope.load();
-			console.log("update task successfully");
-			defer.resolve();
 		});
-		return defer.promise; 
 	}
-	// $scope.insert= function(){
-	// 	$scope.model.new.done = 0;
-	// 	$scope.tasks.push($scope.model.new);
-	// };
-    //
-	// $scope.update = function(){
-	// }
   });
